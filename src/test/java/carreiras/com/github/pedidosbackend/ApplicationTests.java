f8815a65b48d349a342490f771f004d4ac5cefd2
Ewerton Carreira <ewertoncarreira@gmail.com>
2023-05-15 15:02:29 -0300

    Primeiro Commit

---
+++ src/test/java/carreiras/com/github/pedidosbackend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package carreiras.com.github.pedidosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
